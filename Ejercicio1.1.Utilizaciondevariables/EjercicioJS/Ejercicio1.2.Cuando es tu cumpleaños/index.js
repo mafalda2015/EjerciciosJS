25d1a958e6bf5a22202745cc481ce8c694fdf3ed
mafalda2015 <77148545+mafalda2015@users.noreply.github.com>
2021-01-21 12:28:27 +0100

    Ejercicios1.1.

---
+++ Ejercicio1.1.Utilizaciondevariables/EjercicioJS/Ejercicio1.2.Cuando es tu cumpleaños/index.js
@@ -0,0 +1,20 @@
+const celta = {
+    day: 13
+    month: 'May'
+}
+
+console.log('Mi cumpleaños es el' + celta.day + 'de' + celta.month);
+
+const month = ['January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'];
+    
