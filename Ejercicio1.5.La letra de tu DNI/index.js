dc1e329b18810f90b98cbd5e74bfa7d1a1d22c49
mafalda2015 <77148545+mafalda2015@users.noreply.github.com>
2021-01-23 17:04:52 +0100

    Ejercicio 1.6

--- Ejercicio1.5.La letra de tu DNI/index.js
+++ Ejercicio1.5.La letra de tu DNI/index.js
@@ -1,17 +1,19 @@
-const DNI = prompt('Cual es tu DNI');
-
 var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z',
 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
-console.log('DNI', DNI);
+var numero = prompt('Introduce tu numero de DNI');
+var letra = prompt('Introduce la letra de tu DNI')
 
-if (DNI< 0 || DNI>99999999) {
-    alert("numero incorrecto");
+if (numero< 0 || numero>99999999) {
+    alert('numero incorrecto');
 } 
 else {
-    const letrabuscada, letras[DNI%23]
+    var letrabuscada = letras(numero % 23)
     if(letrabuscada != letras)
-    alert('tu letra del DNI es', letrabuscada);
+    alert('la letra o el numero no son correctos');
 }
+else {
+    alert('el numero y letra de tu DNI son correctos')
+}
+console.log('10905847Y', 10905847Y);
 
-const numeros = [1,2,3,4,5,6,7,8,9];
--- Ejercicio1.6.Bucles/index.js
+++ Ejercicio1.6.Bucles/index.js
@@ -1,1 +1,15 @@
 const notas = [6, 7, 2, 9, 3, 4, 5, 8, 2]
+
+// Con un bucle for
+let acc = 0
+for (let i = 0; i < notas.length; i++) {
+    acc += notas[i];
+};
+console.log('con el bucle for la suma total es' + acc);
+
+// Con un bucle for...of
+let sumatotal
+for (let i = 0; i < notas.length; i++) {
+    sumatotal += notas[i];   
+};
+console.log('con el bucle for...of la suma total es' + sumatotal);
