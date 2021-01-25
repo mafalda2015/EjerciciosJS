cb22b0a32ae412e6f69360a64dd54c51689c0941
mafalda2015 <77148545+mafalda2015@users.noreply.github.com>
2021-01-25 17:36:19 +0100

    Ejercicio1.13-1.14-1.15

--- Ejercicio1.13.Concatenacion de metodos/index.js
+++ Ejercicio1.13.Concatenacion de metodos/index.js
@@ -1,7 +1,10 @@
 const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
 const longermonths = months
-    .filter(months => months.length > 7);
-    .map(eachmonths => eachmonths.toUpperCase())
+.filter(months => months.length > 7);
+months.map(eachmonths => eachmonths.toUpperCase());
+    
+console.log('longermonths', longermonths);
 
-console.log('longermonts', longermonths);
+const numberofmonths = longermonths.length;
+console.log('numberofmonths', numberofmonths)
---
+++ Ejercicio1.14. Usuarios premium/index.js
@@ -0,0 +1,17 @@
+const users = [
+    {username: 'ppc90', age: 30, premium: false},
+    {username: 'lu65', age: 24, premium: true},
+    {username: 'maria3', age: 36, premium: false},
+    {username: 'abc123', age: 20, premium: false},
+    {username: 'sergio58', age: 26, premium: true}
+];
+
+users.map(users => {
+    if(users.true) {
+        return enhorabuena
+}
+});
+
+const isnotpremium = users.filter(users => users.false)
+console.log('isnotpremium', isnotpremium);
+
