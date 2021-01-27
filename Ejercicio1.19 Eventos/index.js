301da231695c541c4d69ec3071b0fbef075abc6a
mafalda2015 <77148545+mafalda2015@users.noreply.github.com>
2021-01-27 18:02:58 +0100

    Ejercicio1.20

---
+++ Ejercicio1.19 Eventos/index.js
@@ -0,0 +1,23 @@
+// Al hacer click sobre el boton 'Guardar' aparece un mensaje de alert
+const boton = document.querySelector('button');
+
+boton.addEventListener("click", save);
+
+function save() {
+    alert('datos guardados');
+};
+
+// Al hacer foco sobre input del nombre cambia de color
+const inputnombre = document.querySelector(".name");
+
+inputnombre.addEventListener();
+function changecolorfocus() {
+
+}
+inputnombre.addEventListener();
+function changecolorfocus() {
+
+}
+
+
+// Si la letra pulsada es vocal, el segundo input aparecera en rojo y si es una consonate en verde
---
+++ Ejercicio1.20 Mas eventos/index.js
@@ -0,0 +1,30 @@
+// Al hacer click sobre el boton 'Guardar' aparece un mensaje de alert
+
+function savedata() {
+    alert('datos guardados');
+};
+
+
+// Al hacer foco sobre input del nombre cambia de color
+const inputnombre = document.querySelector(".name");
+
+function changecolorfocus() {
+    inputnombre.style.backgroundcolor = '#0000ff';
+};
+
+function changecolornofocus() {
+    inputnombre.style.backgroundcolor = '#ffa500';
+};
+
+// Si la letra pulsada es vocal, el segundo input aparecera en rojo y si es una consonate en verde
+const inputrandom = document.querySelector(".random");
+
+function checkletter(event) {
+    const currentletter = event.which;
+    if (currentletter === 67 || currentletter === 102 || currentletter === 55 || currentletter === 202) {
+        inputrandom.style.color = '#ff0000';   
+    }   else{
+        inputrandom.style.color = '#008000';
+    }
+};
+
