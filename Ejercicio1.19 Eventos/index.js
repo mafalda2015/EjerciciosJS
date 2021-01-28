bac8c2d2330b651aa99b4fff0e699bee346f65ea
mafalda2015 <77148545+mafalda2015@users.noreply.github.com>
2021-01-28 16:46:46 +0100

    Ejercicio1.19

--- Ejercicio1.19 Eventos/index.js
+++ Ejercicio1.19 Eventos/index.js
@@ -1,8 +1,5 @@
 // Al hacer click sobre el boton 'Guardar' aparece un mensaje de alert
-const boton = document.querySelector('button');
-
-boton.addEventListener("click", save);
-
+document.querySelector('.save').addEventListener("click", save);
 function save() {
     alert('datos guardados');
 };
@@ -10,14 +7,21 @@ function save() {
 // Al hacer foco sobre input del nombre cambia de color
 const inputnombre = document.querySelector(".name");
 
-inputnombre.addEventListener();
 function changecolorfocus() {
-
+    inputnombre.style.backgroundcolor = '#0000ff';
 }
-inputnombre.addEventListener();
-function changecolorfocus() {
-
+function changecolornofocus() {
+    inputnombre.style.backgroundcolor = '#ffa500';
 }
-
 
 // Si la letra pulsada es vocal, el segundo input aparecera en rojo y si es una consonate en verde
+const inputRandom = document.querySelector(".Random");
+
+function checkletter(event) {
+    const currentletter = event.which;
+    if (currentletter === 67 || currentletter === 102 || currentletter === 55 || currentletter === 202) {
+        inputRandom.style.color = '#ff0000';   
+    }   else{
+        inputRandom.style.color = '#008000';
+    }
+};
